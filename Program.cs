e51ec6cee3e0410597751e73734ea2b2b95c258d
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-09 17:31:31 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStaticClasses
+{
+    class Program
+    {
+
+
+        //here is our main method
+        static void Main(string[] args)
+        {
+
+            //Song is the class that we created that has the constructor method that allows us to assign values to the properties of these objects like this
+
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.label);
+            //getSongCount is another method we made in our Song.cs file
+            //the static attribute allows us to assign a value such as the song count to everything inside the class as opposed to the individual objects in the class
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.getSongCount());
+
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStaticClasses
+{
+    class Song
+    {
+
+        //our objects created from our Song class has these following properties
+        public string title;
+        public string artist;
+        public int duration;
+
+
+        //but this property applies to the entire class
+        public static int songCount = 0;
+        public static string label = "RockSteady";
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist; 
+            duration = aDuration;
+            songCount++;
+            
+        }
+
+        public int getSongCount()
+        {
+            return songCount;
+        }
+        public string songLabel()
+        {
+            return songLabel();
+        }
+    }
+}
